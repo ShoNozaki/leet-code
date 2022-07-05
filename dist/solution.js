6e2fac93aa19842edebb8c7537d0b4be2f32446d
Sho Nguyen <shonguyen@shos-mbp.lan>
2022-07-05 12:39:41 -0700

    Jest Typescript setup

---
+++ dist/solution.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const containsDuplicate = (nums) => {
+    // const numberKey: {[num: number] : boolean} = {}
+    // nums.forEach(num => {
+    //     if(numberKey[num]){
+    //         return true;
+    //     } else {
+    //         numberKey[num] = true;
+    //     }
+    //     console.log(numberKey)
+    // })
+    return false;
+};
+exports.default = containsDuplicate;

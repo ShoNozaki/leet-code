1f4c594dabf725e1b1e05b357b058e95793c653f
Sho Nguyen <shonguyen@shos-mbp.lan>
2022-07-05 22:31:13 -0700

    Typescript Jest Setup

---
+++ src/arrays-and-hashing/contains-duplicate/contains-duplicate.test.ts
@@ -0,0 +1,21 @@
+import containsDuplicate from "./solution";
+
+test("should pass", () => {
+  expect(true).toBe(true)
+})
+test('should return false when passed an array of different numbers', () => {
+  const input = [1,2,3]
+  const output = containsDuplicate(input)
+  console.log({input, output})
+  expect(output).toBe(false);
+});
+
+// it("should return false when passed an array of different numbers", () => {
+//   const input = [1,2,3]
+//   const output = containsDuplicate(input)
+//   console.log({input, output})
+//   expect(output).toBe(false);
+// })
+// test('should return true if duplicate number is in array'), () => {
+//   expect(containsDuplicate([1,2,2])).toBe(true)
+// }
---
+++ src/arrays-and-hashing/contains-duplicate/solution.ts
@@ -0,0 +1,14 @@
+const containsDuplicate = (nums: number[]): boolean => {
+    // const numberKey: {[num: number] : boolean} = {}
+    // nums.forEach(num => {
+    //     if(numberKey[num]){
+    //         return true;
+    //     } else {
+    //         numberKey[num] = true;
+    //     }
+    //     console.log(numberKey)
+    // })
+    return false;
+};
+
+export default containsDuplicate

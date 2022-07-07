d8abf206ab6e8647978d7d5df773e9128faa872b
Sho Nguyen <shonguyen@shos-mbp.lan>
2022-07-07 13:43:33 -0700

    Solution for contains duplicate problem

--- src/arrays-and-hashing/contains-duplicate/contains-duplicate.test.ts
+++ src/arrays-and-hashing/contains-duplicate/contains-duplicate.test.ts
@@ -9,13 +9,7 @@ test('should return false when passed an array of different numbers', () => {
   console.log({input, output})
   expect(output).toBe(false);
 });
-
-// it("should return false when passed an array of different numbers", () => {
-//   const input = [1,2,3]
-//   const output = containsDuplicate(input)
-//   console.log({input, output})
-//   expect(output).toBe(false);
-// })
-// test('should return true if duplicate number is in array'), () => {
-//   expect(containsDuplicate([1,2,2])).toBe(true)
-// }
+test('should return true if duplicate number is in array', () => {
+  expect(containsDuplicate([1,2,2])).toBe(true)
+  expect(containsDuplicate([2,1,2])).toBe(true)
+});
--- src/arrays-and-hashing/contains-duplicate/solution.ts
+++ src/arrays-and-hashing/contains-duplicate/solution.ts
@@ -1,14 +1,34 @@
+// Solution 1:
+// Iterate over all values and store into a hash to check against
+// T = (O)n
+// S = (O)n 
+// const containsDuplicate = (nums: number[]): boolean => {
+//     // const numberKey: {[num: number] : boolean} = {}
+
+//     for(let i = 0; i < nums.length; i++){
+//         const val = nums[i]
+//         if(numberKey[val]){
+//             return true;
+//         } else {
+//             numberKey[val] = true;
+//         }
+//     }
+//     return false;
+// };
+
+// Solution 2
+// Use a Set intead of a hash map
+
 const containsDuplicate = (nums: number[]): boolean => {
-    // const numberKey: {[num: number] : boolean} = {}
-    // nums.forEach(num => {
-    //     if(numberKey[num]){
-    //         return true;
-    //     } else {
-    //         numberKey[num] = true;
-    //     }
-    //     console.log(numberKey)
-    // })
+    const numberKey=  new Set()
+    for(let i = 0; i < nums.length; i++){
+        const val = nums[i]
+        if(numberKey.has(val)){
+            return true;
+        } else {
+            numberKey.add(val)
+        }
+    }
     return false;
 };
-
 export default containsDuplicate

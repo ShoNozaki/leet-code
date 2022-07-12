fe85872d005a1d9c79f61718378436af4da0d226
Sho Nguyen <shonguyen@shos-mbp.lan>
2022-07-12 14:33:45 -0700

    Solution for valid-palindrome

--- src/two-pointers/valid-palindrome/solution.ts
+++ src/two-pointers/valid-palindrome/solution.ts
@@ -1,6 +1,90 @@
 // solution 1
+// make all lowercase, remove non alphanumerics
 // use pointers and check if they are alphanumeric, then check if match. 
+// T = O(n)
+// S = O(n)
+// n = characters in string 
+
+export const isAlphanumeric = (s: string): boolean => {
+    const lettersNumbers = /^[a-zA-Z0-9]+$/
+    if(s.match(lettersNumbers)){
+        return true
+    } 
+    return false
+}
+
+// interface LetterNumberMap {
+//     [char: string | number] : boolean
+// }
+// export const isAlphanumeric = (s: string): boolean => {
+//     const alphaNumericMap: LetterNumberMap = {
+//         a: true,
+//         b: true,
+//         c: true,
+//         d: true,
+//         e: true,
+//         f: true,
+//         g: true,
+//         h: true,
+//         i: true,
+//         j: true,
+//         k: true,
+//         l: true,
+//         m: true,
+//         n: true,
+//         o: true,
+//         p: true,
+//         q: true,
+//         r: true,
+//         s: true,
+//         t: true,
+//         u: true,
+//         v: true,
+//         w: true,
+//         x: true,
+//         y: true,
+//         z: true,
+//         0: true,
+//         1: true,
+//         2: true,
+//         3: true,
+//         4: true,
+//         5: true,
+//         6: true,
+//         7: true,
+//         8: true,
+//         9: true,
+        
+//     }
+//     if(alphaNumericMap[s]){
+//         return true
+//     } 
+//         return false
+// }
+
 const isPalindrome =(s: string): boolean => {
+    let startPoint = 0
+    let endPoint = s.length - 1
+    while(startPoint <= endPoint){
+        let startChar = s[startPoint].toLowerCase()
+        let endChar = s[endPoint].toLowerCase()
+        // check ascii
+        if(!isAlphanumeric(startChar)){
+            startPoint+= 1;
+        }
+        if(!isAlphanumeric(endChar)){
+            endPoint-= 1;
+        }
+        if(isAlphanumeric(startChar) && isAlphanumeric(endChar)){
+            // check match
+            if(startChar === endChar){
+                startPoint+=1
+                endPoint-=1
+            }else {
+                return false
+            }
+        }
+    }
     return true
 };
 
--- src/two-pointers/valid-palindrome/valid-palindrome.test.ts
+++ src/two-pointers/valid-palindrome/valid-palindrome.test.ts
@@ -1,8 +1,35 @@
-import isPalindrome from './solution'
+import isPalindrome, {isAlphanumeric} from './solution'
+
+describe("isAlphanumeric", () => {
+    it("should return true", ()=>{
+        expect(isAlphanumeric("aaa")).toBe(true)
+    })
+    it("should return true", ()=>{
+        expect(isAlphanumeric("Abc")).toBe(true)
+    })
+    it("should return false", ()=>{
+        expect(isAlphanumeric("!")).toBe(false)
+    })
+    it("should return false", ()=>{
+        expect(isAlphanumeric("a!A")).toBe(false)
+    })
+})
 
 describe("isPalindrome", ()=>{
-    it("should return true", ()=>{
+    it("should return false is s in not a palindrome", ()=>{
         const s = "string"
+        expect(isPalindrome(s)).toBe(false)
+    })
+    it("should return true is s is a palindrome", ()=>{
+        const s = "catac"
+        expect(isPalindrome(s)).toBe(true)
+    })
+    it("should return true is s is a palindrome", ()=>{
+        const s = "cat!ac"
+        expect(isPalindrome(s)).toBe(true)
+    })
+    it("should return true", ()=>{
+        const s = "A man, a plan, a canal: Panama"
         expect(isPalindrome(s)).toBe(true)
     })
 })
